1487e79dd50af8f0f15971131266100fa79300bb
D.H.D. Nguyen <jasmine.1995dn@gmail.com>
2020-05-03 18:42:29 +0200

    my solution for question 4 and 6 - week 1

---
+++ week1/question_6_jung.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(-5, 5, 0.1)
+y = np.arange(-5, 5, 0.1)
+
+xx, yy = np.meshgrid(x, y, sparse=True)
+
+# function f(x,y)
+z = np.sin(6*np.cos(np.sqrt(xx**2+yy**2)) + 5*np.arctan2(yy,xx))
+
+# plot
+fig = plt.contourf(x,y,z, cmap=plt.get_cmap('gnuplot2') )
+
+
+plt.show()
